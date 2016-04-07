631383eed0d1db0379f40f3e67c35dc65cb652ca
David Norminton <davidnorminton@gmail.com>
2016-04-07 02:08:08 +0100

    Added files via upload

---
+++ htpasswd-handler/public/partials/htpasswd-handler-public-display.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://davenorm.me
+ * @since      1.0.0
+ *
+ * @package    Htpasswd_Handler
+ * @subpackage Htpasswd_Handler/public/partials
+ */
+
