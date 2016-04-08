e3e1ba6093173fe81ccfd8611b52dea6ba6462cd
David Norminton <davidnorminton@gmail.com>
2016-04-09 00:29:25 +0100

    Delete htpasswd-adduser.php

--- htpasswd-handler/htpasswd-adduser.php
+++
@@ -1,9 +0,0 @@
-<?php    
-/**
- * add a user to htpasswd file
- */
-// build string to be stored in htpasswd file
-$new_user = $_POST['username'] . ':' . encryptPassword($_POST['password']). "\n";
-    
-if (file_put_contents(HTPASSWD_PATH, $new_user, FILE_APPEND))
-    echo '<div class="output"><span class="added">' . $_POST['username'] . '</span> has been added to your .htpasswd file</div>'; 
